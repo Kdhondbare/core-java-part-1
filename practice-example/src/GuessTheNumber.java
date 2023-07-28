4a903d55b705ce52599ff3ddc788523f8b384260
Karan Dhondbare <kdhondbare123@gmail.com>
2023-07-28 19:25:32 +0530

    modifying guess the number game

--- practice-example/src/GuessTheNumber.java
+++ practice-example/src/GuessTheNumber.java
@@ -1,51 +1,56 @@
 import java.util.Random;
 import java.util.Scanner;
 
-class Game{
+class Game {
     public int number;
     public int inputNumber;
-    public int numberOfGuesses;
+    public int noOfGuesses = 0;
 
-    public int getNumberOfGuesses() {
-        return numberOfGuesses;
+    public int getNoOfGuesses() {
+        return noOfGuesses;
     }
 
-    public void setNumberOfGuesses(int numberOfGuesses) {
-        this.numberOfGuesses = numberOfGuesses;
+    public void setNoOfGuesses(int noOfGuesses) {
+        this.noOfGuesses = noOfGuesses;
     }
 
-    void Game (){
-        Random r = new Random();
-        this.number = r.nextInt(100);
-    }
-    void takeUserInput(){
-        System.out.println("Guess The Number");
-        Scanner s = new Scanner(System.in);
-        inputNumber = s.nextInt();
 
+    Game() {
+        Random rand = new Random();
+        this.number = rand.nextInt(100);
     }
-    boolean isCorrectNumber(int num){
-        if(num == number){
+
+    void takeUserInput() {
+        System.out.println("Guess the number");
+        Scanner sc = new Scanner(System.in);
+        inputNumber = sc.nextInt();
+    }
+
+    boolean isCorrectNumber() {
+        noOfGuesses++;
+        if (inputNumber == number) {
+            System.out.format("Yes you guessed it right, it was %d\nYou guessed it in %d attempts", number, noOfGuesses);
             return true;
-        } else if (num<number) {
-            System.out.println("Too less....");
-        }else if (num>number) {
-            System.out.println("Too high....");
+        } else if (inputNumber < number) {
+            System.out.println("Too low...");
+        } else if (inputNumber > number) {
+            System.out.println("Too high...");
+        }
         return false;
-
     }
-
 }
 
 
-public class GuessTheNumber {
+    public class GuessTheNumber {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        Game g = new Game();
-        g.takeUserInput();
-        boolean b = g.isCorrectNumber()
+            Game g = new Game();
+            boolean b = false;
+            while (!b) {
+                g.takeUserInput();
+                b = g.isCorrectNumber();
+            }
+        }
+
     }
-
-
-}
