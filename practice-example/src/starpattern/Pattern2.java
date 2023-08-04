3186c49e291e9d181b42eae0c7286e5c3c3a8bf5
Karan Dhondbare <kdhondbare123@gmail.com>
2023-08-04 21:51:54 +0530

    adding star pattern practice codes

--- practice-example/src/starpattern/Pattern2.java
+++ practice-example/src/starpattern/Pattern2.java
@@ -15,5 +15,17 @@ public class Pattern2 {
             }
             System.out.println();
         }
+
+        for (int i=1; i<=5; i++) {
+
+            for (int j=2; j<=i; j++) {
+                System.out.print(" ");
+            }
+            for (int k=5; k>=i; k--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
