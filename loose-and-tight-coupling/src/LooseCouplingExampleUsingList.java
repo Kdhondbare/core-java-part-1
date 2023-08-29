3700265a8971001ef5d78d9e330de59c7b9ec88c
Karan Dhondbare <kdhondbare123@gmail.com>
2023-08-29 22:15:39 +0530

    updating loose and tight coupling example.

--- loose-and-tight-coupling/src/LooseCouplingExampleUsingList.java
+++ loose-and-tight-coupling/src/LooseCouplingExampleUsingList.java
@@ -1,14 +1,30 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class LooseCouplingExampleUsingList {
 
     public static void main(String[] args) {
 
-        ArrayList<String> guestList = new ArrayList<>();//tight coupling
+       // ArrayList<String> guestList = new ArrayList<>();//tight coupling
 
-        LinkedList<String> guestList1 = new LinkedList<>();//tight coupling
+        LinkedList<String> guestList = new LinkedList<>();//tight coupling
+        // trying to change ArrayList to LinkedList for above guestList
 
+        guestList.add("Sahil");
+        guestList.add("Nikhil");
+        guestList.add("Pratik");
+        //guestList.trimToSize(); We have to comment this method call
+        //because this method is only written in ArrayList and not in LinkedList
+        //this method is written in ArrayList only.
+        //if we try to use LinkedList instead of ArrayList object then
+        //we will have to remove this calling method "trimToSize()"
+
+
+        //List<String> newGuestList = new ArrayList<>();//loose coupling
+        List<String> newGuestList = new LinkedList<>();
+        newGuestList.add("Vivek");
+        newGuestList.add("Prathmesh");
 
     }
 }
