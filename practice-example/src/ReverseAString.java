fe7aad561021e7eb918ec16a5fe96dfbcacff7d8
Karan Dhondbare <kdhondbare123@gmail.com>
2023-09-11 22:12:06 +0530

    updating Reverse A String code.

--- practice-example/src/ReverseAString.java
+++ practice-example/src/ReverseAString.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class ReverseAString {
 
     public static void main(String[] args) {
-        String name = "Karan";
+        System.out.println("Reverse a string program.");
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter a String...");
+        String name = s.next();
         int length = name.length();
         String rev = "";
         for (int i=length-1; i>=0; i--) {
