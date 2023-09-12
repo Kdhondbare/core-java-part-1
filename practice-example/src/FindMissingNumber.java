190d2e36dd0fa4c862e1249265b07912300d0d45
Karan Dhondbare <kdhondbare123@gmail.com>
2023-09-12 23:43:56 +0530

    adding Find Missing Number code.

---
+++ practice-example/src/FindMissingNumber.java
@@ -0,0 +1,17 @@
+public class FindMissingNumber {
+
+    public static void main(String[] args) {
+
+        int[] a = {1, 2, 3, 4, 6};
+
+        int expectedNumberElements = a.length + 1;
+        int totalSum = expectedNumberElements * (expectedNumberElements + 1) / 2;
+        //System.out.println(totalSum);
+
+        int sum = 0;
+        for (int i=0; i<a.length; i++) {
+            sum = sum + a[i];
+        }
+        System.out.println("Missing number is : " + (totalSum - sum));
+    }
+}
